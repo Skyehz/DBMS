793f1cd99dc758634b19e81943baa699c62d59ea
Skyehz <czh15296700133@163.com>
2021-11-25 15:32:25 +0800

    修改sql和刷新id

--- resource.h
+++ resource.h
@@ -15,13 +15,15 @@
 #define ID_32774                        32774
 #define ID_32775                        32775
 #define ID_32776                        32776
+#define ID_32880                        32777
+#define ID_32881                        32778
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        314
-#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_COMMAND_VALUE         32779
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
